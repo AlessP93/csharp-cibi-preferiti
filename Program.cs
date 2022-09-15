8297cd329b6df4e18c9336470a7a9b21adb508ae
AlessP93 <alespiscino@libero.it>
2022-09-15 16:03:35 +0200

    primo commit, esercizio finito

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//creo un array 
+string[] food = { "pizza", "lasagna", "gelato", "tiramisù" };
+
+var first = food[1]; //lasagna
+var middle = food[2]; //gelato
+var last = food[3]; //tiramisù
+
+for (int i = 0; i < food.Length; i++)
+
+System.Console.WriteLine(food[i]);
+System.Console.WriteLine(first);
+System.Console.WriteLine(middle);
+System.Console.WriteLine(last);
+
+
+//stringArray.First() and stringArray.Last()
