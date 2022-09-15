21de138060fdda4319f216967468560125c8d45a
AlessP93 <alespiscino@libero.it>
2022-09-15 16:03:35 +0200

    primo commit, esercizio finito fix classifica

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//creo un array 
+string[] food = { "pizza", "lasagna", "gelato", "tiramisù" };
+
+//lunghezza array 4
+int arraylength = food.Length;
+
+System.Console.WriteLine(arraylength);
+
+//cibi preferiti in oridine 
+var first = food[1]; //lasagna
+var middle = food[2]; //gelato
+var last = food[3]; //tiramisù
+
+for (int i = 0; i < food.Length; i++)
+
+System.Console.WriteLine(food[i]);
+System.Console.WriteLine(first);
+System.Console.WriteLine(middle);
+System.Console.WriteLine(last);
+
+
+//stringArray.First() and stringArray.Last()
